24733f64bc259cc4306079a91aae25a00e1d9fec
eslam hassan <hassaneslam612@gmail.com>
2025-05-12 22:46:09 +0300

    Add project files.

---
+++ Simpe/Profile/SimpleMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simpe.Profile
+{
+    public static class SimpleMapper 
+    {
+        public static TDestination Map<TSource, TDestination>(TSource source) where TDestination : new()
+        {
+            TDestination destination = new TDestination();
+            var sourceProps = typeof(TSource).GetProperties();
+            var destProps = typeof(TDestination).GetProperties();
+
+            foreach (var sourceProp in sourceProps)
+            {
+                var matchingDestProp = destProps.FirstOrDefault(p => p.Name == sourceProp.Name && p.PropertyType == sourceProp.PropertyType);
+                if (matchingDestProp != null && matchingDestProp.CanWrite) 
+                {
+                    var value = sourceProp.GetValue(source);
+                    matchingDestProp.SetValue(destination, value);
+                }
+            }
+            return destination;
+
+        }
+    }
+}
---
+++ Simpe/Program.cs
@@ -0,0 +1,17 @@
+﻿using Simpe.Profile;
+using Simple.Entity;
+
+namespace Simpe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dto = new PersonDto { Name = "Ahmed", Age = 25 };
+
+            var person = SimpleMapper.Map<PersonDto, Person>(dto);
+
+            Console.WriteLine($"Name {person.Name} : \n  age {person.Age}");
+        }
+    }
+}
